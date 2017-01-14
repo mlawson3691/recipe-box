29156909e946121004239c1cd9fa7813887bcb97
Mark Lawson <mlawson3691@gmail.com>
2017-01-14 12:02:58 -0800

    link components in App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,58 @@
 import React, { Component } from 'react';
+import ListComponent from './components/ListComponent/App.js';
+import DisplayComponent from './components/DisplayComponent/App.js';
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedRecipe: {
+        'name': 'Brownies',
+        'ingredients': ['butter', 'sugar', 'eggs', 'flour', 'chocolate'],
+        'instructions': ['mix butter and sugar', 'add eggs', 'stir in flour and chocolate', 'bake']
+      },
+      recipes: [
+        {
+          'name': 'Brownies',
+          'ingredients': ['butter', 'sugar', 'eggs', 'flour', 'chocolate'],
+          'instructions': ['mix butter and sugar', 'add eggs', 'stir in flour and chocolate', 'bake']
+        },
+        {
+          'name': 'Mixed Berry Smoothie',
+          'ingredients': ['strawberries', 'blueberries', 'raspberries', 'yogurt', 'honey'],
+          'instructions': ['add all ingredients to blender', 'blend until smooth']
+        },
+        {
+          'name': 'Rustic Bread',
+          'ingredients': ['yeast', 'water', 'sugar', 'salt', 'flour'],
+          'instructions': ['mix yeast, water, and sugar', 'add flour and salt', 'knead for 10 min', 'proof dough', 'form and bake']
+        },
+        {
+          'name': 'Veggie Fried Rice',
+          'ingredients': ['rice', 'soy sauce', 'eggs', 'onion', 'carrot', 'zucchini', 'mushrooms', 'butter'],
+          'instructions': ['cook rice', 'stir fry remaining ingredients', 'add rice to stir fry', 'season with soy sauce and add butter']
+        }
+      ]
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <h2>Recipe Box</h2>
+          <p>Recipe Box</p>
+        </div>
+        <div className="App-body">
+          <ListComponent
+            recipes={this.state.recipes}
+          />
+          <DisplayComponent
+            selectedRecipe={this.state.selectedRecipe}
+          />
         </div>
       </div>
     );
   }
 }
-
-export default App;
